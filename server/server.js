c8c1025831c2ced5f1956708dad28bd98477d441
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-04-17 12:02:25 +0300

    Add io & socket

--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
 const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./app');
+const { Message } = require('./models');
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('=======>>>  connection to socket');
+  socket.on('newMessage', async (newMessage) => {
+    try {
+      console.log('newMessage =======>>> ', newMessage);
+      const saveMessage = await Message.create(newMessage);
+      io.emit('newMessage', saveMessage);
+    } catch (error) {
+      socket.emit('errorMessage', error);
+    }
+  });
+  socket.on('disconnect', (reason) => {
+    console.log('reason =======>>> ', reason);
+  });
+});
+
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
